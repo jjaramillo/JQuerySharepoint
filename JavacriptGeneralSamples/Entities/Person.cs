03ef9dc7dfb81e18870faf3f6a67ab9db35e301e
Jaime Jaramillo <jjaramillozapata@icloud.com>
2013-12-03 17:48:52 -0500

    -Ejemplos de llamados de ajax -Implementacion de servicio wcf est

---
+++ JavacriptGeneralSamples/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JavacriptGeneralSamples.Entities
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+    }
+}
